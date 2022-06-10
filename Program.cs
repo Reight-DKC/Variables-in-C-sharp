e572602391aa4cda1cead18acb522f0dcd634987
Reight-DKC <reightultimate@gmail.com>
2022-06-10 11:15:36 -0600

    First variables learned

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Variables");
+
+// int
+int age = 21;
+Console.WriteLine(age);
+
+int roomNumber;
+roomNumber = 4;
+Console.WriteLine(roomNumber);
+
+// double
+double pi = 3.14D;
+Console.WriteLine(pi);
+
+// char
+char firstLetter = 'A';
+Console.WriteLine(firstLetter);
+
+// bool
+bool accepted = false;
+Console.WriteLine(accepted);
+
+bool complete = true;
+Console.WriteLine(complete);
+
+// string
+string lastName = "Portocarrero";
+Console.WriteLine(lastName);
+
+// Programa de lectura y escritura I/O
+
+string fullName;
+Console.WriteLine("Ingrese su nombre completo.");
+fullName = Console.ReadLine();
+
+Console.Write("Su nombre es: ");
+Console.Write(fullName);
+
+// ------
+Console.Write("\n¿En que país vive usted? ");
+string country = Console.ReadLine();
+
+Console.WriteLine("Su país es ");
+Console.WriteLine(country);
